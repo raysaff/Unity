4543625c17f4b4794f8338e111d207e2a1aaa90c
Ray <sav777@yandex.ru>
2021-03-16 06:43:57 +0700

    Experiments Added

---
+++ Experiments/Assets/Scenes/ScaleAnimationByCurves.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleAnimationByCurves : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve _myCurve = null;
+
+    private void OnMouseDown()
+    {
+        StartCoroutine(AnimationScale());
+    }
+
+    IEnumerator AnimationScale()
+    {
+        for (float i=0; i<1; i+=Time.deltaTime)
+        {
+            
+            transform.localScale = Vector3.one * _myCurve.Evaluate(i);
+            yield return null;
+        }
+
+        transform.localScale = Vector3.one * _myCurve.Evaluate(1f);
+    }
+}
